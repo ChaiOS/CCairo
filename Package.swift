512c79a0d5f9859d1f831c3138016e50d969eab2
Abdurrahman Avci <abdurrahmanavci@gmail.com>
2019-05-14 22:55:00 +0300

    Switch to swift tools version 4.2.

--- Package.swift
+++ Package.swift
@@ -1,18 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "CCairo",
-    pkgConfig: "cairo",
-    providers: [
-        .apt(["libcairo2-dev"]),
-        .brew(["cairo"])
-    ],
     products: [
         .library(name: "CCairo", targets: ["CCairo"])
     ],
     targets: [
-        .target(name: "CCairo")
+        .systemLibrary(
+            name: "CCairo",
+            pkgConfig: "cairo",
+            providers: [
+                .apt(["libcairo2-dev"]),
+                .brew(["cairo"])
+            ]
+        )
     ]
 )
